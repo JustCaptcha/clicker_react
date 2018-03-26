9c3c519bd40115707072fd8c569740166a98e234
Albert <higashi166@gmail.com>
2018-03-26 02:33:43 +0200

    Hot fix

--- src/components/Navbar/Options/index.js
+++ src/components/Navbar/Options/index.js
@@ -1,36 +1,9 @@
 import React, { Component } from 'react'
 import Cookies from 'js-cookie'
 import './index.css'
-// import { Button } from '../../../containers/Button';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux'
 import { saveCookies } from '../../../store/actions/cookies'
-
-/* export class Options extends Component {
-  render() {
-    return (
-      <div>
-        <button onClick={this.saveCookies}>Options</button>
-        <button onClick={this.getCookies}>Get</button>
-        <button onClick={this.deleteCookies}>Delete</button>
-      </div>
-    )
-  }
-  saveCookies() {
-    console.log('SAVE')
-    Cookies.set('Alberto', 'Cool!');
-  }
-  getCookies() {
-    console.log('GET')
-    alert(Cookies.get('Alberto'))
-  }
-  deleteCookies() {
-    console.log('DELETE')
-    Cookies.remove('Alberto')
-  }
-}
-
-export default Options */
 
 export class Options extends Component {
   render() {
@@ -43,10 +16,6 @@ export class Options extends Component {
       </div>
     )
   }
-/*   saveCookies() {
-    console.log('SAVE')
-    Cookies.set('Alberto', 'Cool!');
-  } */
   getCookies() {
     console.log('GET')
     alert(Cookies.get('userState'))
